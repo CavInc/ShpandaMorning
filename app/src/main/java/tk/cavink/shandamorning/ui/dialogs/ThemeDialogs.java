da95c9dd15e93f86a1058809f5e3f6c1300ba1e2
Alexander <cavinc20@gmail.com>
2021-08-16 15:40:58 +0700

    Работа над темой. убраны ссылки на WorkManager

--- app/src/main/java/tk/cavink/shandamorning/ui/dialogs/ThemeDialogs.java
+++ app/src/main/java/tk/cavink/shandamorning/ui/dialogs/ThemeDialogs.java
@@ -2,6 +2,7 @@ package tk.cavink.shandamorning.ui.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,12 +15,20 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import tk.cavink.shandamorning.R;
+import tk.cavink.shandamorning.utils.ConstantManager;
 
 /**
  * Created by cav on 16.08.21.
  */
 
 public class ThemeDialogs extends DialogFragment implements View.OnClickListener {
+    private ThemeDialogListener mThemeDialogListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,12 +47,38 @@ public class ThemeDialogs extends DialogFragment implements View.OnClickListener
         getDialog().getWindow().setAttributes(p);
         View v = inflater.inflate(R.layout.theme_dialog,container,false);
         v.setElevation(12);
+
+        v.findViewById(R.id.theme_green).setOnClickListener(this);
+        v.findViewById(R.id.theme_velvet).setOnClickListener(this);
+        v.findViewById(R.id.theme_blue).setOnClickListener(this);
+        v.findViewById(R.id.theme_dark).setOnClickListener(this);
+        v.findViewById(R.id.theme_gray).setOnClickListener(this);
+
         return v;
     }
 
     @Override
     public void onClick(View view) {
-
+        if (mThemeDialogListener != null){
+            switch (view.getId()){
+                case R.id.theme_green:
+                    mThemeDialogListener.onSelect(ConstantManager.THEME_GREEN);
+                    break;
+                case R.id.theme_velvet:
+                    mThemeDialogListener.onSelect(ConstantManager.THEME_VELVET);
+                    break;
+                case R.id.theme_blue:
+                    mThemeDialogListener.onSelect(ConstantManager.THEME_BLUE);
+                    break;
+                case R.id.theme_dark:
+                    mThemeDialogListener.onSelect(ConstantManager.THEME_DARK);
+                    break;
+                case R.id.theme_gray:
+                    mThemeDialogListener.onSelect(ConstantManager.THEME_GRAY);
+                    break;
+            }
+        }
+        dismiss();
     }
 
     /*
@@ -60,5 +95,8 @@ public class ThemeDialogs extends DialogFragment implements View.OnClickListener
     }
 */
 
+    public interface ThemeDialogListener {
+        void onSelect(int theme);
+    }
 
 }
--- app/src/main/java/tk/cavink/shandamorning/utils/ConstantManager.java
+++ app/src/main/java/tk/cavink/shandamorning/utils/ConstantManager.java
@@ -11,4 +11,9 @@ public interface ConstantManager {
 
     int ADD_ALARM = 0;
     int EDIT_ALARM = 1;
+    int THEME_GREEN = 0;
+    int THEME_VELVET = 1;
+    int THEME_BLUE = 2;
+    int THEME_DARK = 3;
+    int THEME_GRAY = 4;
 }
