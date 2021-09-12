97501f1b2e53ff0590b2589d7cc9de1d9bfa5c35
CavInc <cavinc20@gmail.com>
2021-09-12 18:39:52 +0700

    Сделано изменение громкости звука

--- app/src/main/java/tk/cavink/shandamorning/ui/activites/AlarmActivity.java
+++ app/src/main/java/tk/cavink/shandamorning/ui/activites/AlarmActivity.java
@@ -1,6 +1,9 @@
 package tk.cavink.shandamorning.ui.activites;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +17,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.Calendar;
@@ -41,6 +45,8 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
 
     private ImageButton mAlarmStop; // кнопа гашения
     private float storeX;
+    private AudioManager amanager;
+    private int volOld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,6 +123,9 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
     private void startMusic(){
 
         if (urlSound!=null && urlSound.length()!=0) {
+            amanager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+            volOld = amanager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
             Log.d("ALARMA",urlSound);
             try {
                 mMediaPlayer.reset();
@@ -130,7 +139,19 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
 
                 final float volume = (float) (1 - (Math.log(MAX_VOLUME - alarm_volume) / Math.log(MAX_VOLUME)));
 
-                mMediaPlayer.setVolume(alarm_volume/100.0f, alarm_volume/100.0f);
+                //int volune = amanager.getStreamVolume(AudioManager.STREAM_ALARM);
+                //Log.d("ALARMA","ALARM VOLUME: "+volune);
+                //amanager.setStreamVolume(AudioManager.STREAM_ALARM,alarm_volume,AudioManager.FLAG_SHOW_UI + AudioManager.FLAG_PLAY_SOUND);
+
+                //Log.d("ALARMA","MUSIC VOLUME:" + volOld+" MAX:"+amanager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+
+                //amanager.setStreamVolume(AudioManager.STREAM_MUSIC,  alarm_volume,AudioManager.FLAG_SHOW_UI+AudioManager.FLAG_PLAY_SOUND);
+                amanager.setStreamVolume(AudioManager.STREAM_MUSIC,  alarm_volume,AudioManager.FLAG_PLAY_SOUND);
+
+                //mMediaPlayer.setVolume(alarm_volume/100.0f, alarm_volume/100.0f);
+                mMediaPlayer.setVolume(alarm_volume/15.0f,alarm_volume/15.0f);
+
+
                // mMediaPlayer.setVolume(volume,volume);
                 mMediaPlayer.setScreenOnWhilePlaying(true); // не дает уснуть во премя воспроизведениея ?
                 mMediaPlayer.setLooping(true); // зациклим до окончания работы активности
@@ -138,7 +159,10 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
                 mMediaPlayer.start();
             } catch (IOException e) {
                 e.printStackTrace();
-
+                Toast.makeText(this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                amanager.setStreamVolume(AudioManager.STREAM_MUSIC,volOld,AudioManager.FLAG_PLAY_SOUND);
+            } finally {
+                //amanager.setStreamVolume(AudioManager.STREAM_MUSIC,vol,AudioManager.FLAG_SHOW_UI+AudioManager.FLAG_PLAY_SOUND);
             }
         }
 
@@ -146,6 +170,7 @@ public class AlarmActivity extends AppCompatActivity implements View.OnClickList
 
     private void stopMusic(){
         if (mMediaPlayer != null) {
+            amanager.setStreamVolume(AudioManager.STREAM_MUSIC,volOld,AudioManager.FLAG_PLAY_SOUND);
             try {
                 mMediaPlayer.release();
                 mMediaPlayer = null;
