ecf6838238c716e497057f1fa36672f454a3819c
Alexander <cavinc20@gmail.com>
2021-09-15 09:53:26 +0700

    Сделан показ уведомления для A10+ Удален лишний код

--- app/src/main/java/tk/cavink/shandamorning/services/AlarmTaskReciver.java
+++ app/src/main/java/tk/cavink/shandamorning/services/AlarmTaskReciver.java
@@ -1,20 +1,16 @@
 package tk.cavink.shandamorning.services;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
+
 import android.util.Log;
 
-import java.util.Objects;
+
 
 import tk.cavink.shandamorning.App;
-import tk.cavink.shandamorning.R;
+
 import tk.cavink.shandamorning.ui.activites.AlarmActivity;
 import tk.cavink.shandamorning.utils.ConstantManager;
 
@@ -42,18 +38,6 @@ public class AlarmTaskReciver extends BroadcastReceiver {
 
     private void startAlarm(){
         if (mId != -1) {
-            /*
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                startActivityNotification();
-            } else {
-                Intent intent = new Intent(App.getContext(), AlarmActivity.class);
-                intent.putExtra(ConstantManager.ALARM_ID, mId);
-                intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(ConstantManager.VIBRO_ENABLE, true);
-                App.getContext().startActivity(intent);
-            }
-            */
-
             Intent intent = new Intent(App.getContext(), AlarmActivity.class);
             intent.putExtra(ConstantManager.ALARM_ID, mId);
             intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
@@ -63,42 +47,5 @@ public class AlarmTaskReciver extends BroadcastReceiver {
         }
     }
 
-    private void startActivityNotification(){
-        NotificationManager mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder mBuilder;
 
-        Intent intent = new Intent(App.getContext(), AlarmActivity.class);
-        intent.putExtra(ConstantManager.ALARM_ID,mId);
-        intent.setFlags(FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.putExtra(ConstantManager.VIBRO_ENABLE, true);
-
-        PendingIntent ContentPendingIntent = PendingIntent.getActivity(mContext,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-
-        mBuilder = new NotificationCompat.Builder(mContext)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Alarm")
-                .setContentText("alarm")
-                .setColor(mContext.getResources().getColor(R.color.colorPrimaryDark))
-                .setAutoCancel(true)
-                .setContentIntent(ContentPendingIntent)
-                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID,
-                    "Activity Opening Notification",
-                    NotificationManager.IMPORTANCE_HIGH);
-            mChannel.enableLights(true);
-            mChannel.enableVibration(true);
-            mChannel.setDescription("Activity opening notification");
-
-            mBuilder.setChannelId(CHANNEL_ID);
-
-            Objects.requireNonNull(mNotificationManager).createNotificationChannel(mChannel);
-        }
-        Objects.requireNonNull(mNotificationManager)
-                .notify(TAG_NOTIFICATION,NOTIFICATION_ID,mBuilder.build());
-
-    }
 }
